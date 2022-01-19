7f273e1070b09f4e44aef882ce3c47fb77dce15f
Mr-Destructive <gormeet711@gmail.com>
2022-01-19 21:49:01 +0530

    added settings for heroku deployment

--- feeder/views.py
+++ feeder/views.py
@@ -9,5 +9,5 @@ class NewsletterView(ListView):
     def get_context_data(self, **kwargs):
         call_command('startjobs')
         context = super().get_context_data(**kwargs)
-        context["articles"] = Article.objects.filter().order_by("-pub_date")[:12]
+        context["articles"] = Article.objects.filter().order_by("pub_date")[:12]
         return context
--- newsletter/settings.py
+++ newsletter/settings.py
@@ -1,24 +1,18 @@
-
 from pathlib import Path
 import os
+import dj_database_url
+import django_heroku
+from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, 'newsletter\.env'))
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG=True
 
+ALLOWED_HOSTS = ['timezone-api.herokuapp.com','127.0.0.1','localhost:8000']
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5g9$3r^b@pk#pcd-0v&21be))nyf_y!ilg(t8wzv-_2aw--5h*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -64,10 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'newsletter.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -77,10 +67,6 @@ DATABASES = {
         'HOST': 'localhost',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -97,10 +83,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -112,17 +94,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
 
 EMAIL_BACKEND = 'mailer.backend.DbBackend'
 EMAIL_FILE_PATH = '/templates/' 
